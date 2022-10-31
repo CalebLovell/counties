af15c0acae685c1508acb3ebb0dbc90315b1d853
CalebLovell <caleblovell1@gmail.com>
2022-10-31 17:05:27 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			height: {
+				content: `var(--h-content-area)`,
+			},
+			minHeight: {
+				content: `var(--h-content-area)`,
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/forms'), require('flowbite/plugin')],
+};
