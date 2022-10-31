dff644013e0832bef680e7c474be37466b7a7e75
CalebLovell <caleblovell1@gmail.com>
2022-10-31 18:27:46 +0100

    install packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
+import Map from './components/Map';
+
 function App() {
-	return (
-		<div className='App'>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-		</div>
-	);
+	return <Map />;
 }
 
 export default App;
