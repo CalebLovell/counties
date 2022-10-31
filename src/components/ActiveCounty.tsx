d54cf169a5fa9c173585305c42b8145d4af4e116
CalebLovell <caleblovell1@gmail.com>
2022-10-31 18:27:56 +0100

    add old broken components

---
+++ src/components/ActiveCounty.tsx
@@ -0,0 +1,41 @@
+export const ActiveCounty = ({ county: { activeCounty } }) => {
+	const { county_id, county_name, county_state_name, household_income, property_value, commute_time, median_age, avg_temp } = activeCounty;
+	return county_id ? (
+		<div className='active-county'>
+			<div className='title-bar'>
+				<h1>
+					{county_name}, {county_state_name}
+				</h1>
+			</div>
+			<div className='county-data'>
+				<div className='county-data-title'>Avg Temperature</div>
+				{avg_temp ? (
+					<p>
+						{Math.round(avg_temp)}
+						<span>&#176;</span>F
+					</p>
+				) : (
+					<p>-</p>
+				)}
+			</div>
+			<div className='county-data'>
+				<div className='county-data-title'>Household Income</div>
+				<p>${Math.round(household_income / 1000) * 1000}</p>
+			</div>
+			<div className='county-data'>
+				<div className='county-data-title'>Property Value</div>
+				<p>${Math.round(property_value / 1000) * 1000}</p>
+			</div>
+			<div className='county-data'>
+				<div className='county-data-title'>Commute Time</div>
+				<p>{Math.round(commute_time)} minutes</p>
+			</div>
+			<div className='county-data'>
+				<div className='county-data-title'>Median Age</div>
+				<p>{Math.round(median_age)} years</p>
+			</div>
+		</div>
+	) : (
+		<div className='active-county' />
+	);
+};
